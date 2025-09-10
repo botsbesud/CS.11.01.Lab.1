b2ede7a76360ba01430537ab6796601ee12b02bc
thomaschen <thomasc2028@student.cis.edu.hk>
2025-09-11 07:45:02 +0800

    done

--- src/DateableTwo.java
+++ src/DateableTwo.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
-public class Dateable {
+public class DateableTwo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner s = new Scanner(System.in);
         System.out.println("Please enter an age: ");
         int myInt = s.nextInt();
         int theirAge = myInt/2+7;
         System.out.println(myInt + "-year olds should date someone who is at least " + theirAge + "-years old");
-        }
     }
+}
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
 
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter an age:");
+        int myInt = s.nextInt();
+        int totalPages = 100-myInt;
+        System.out.println(myInt + "-year old should read at least " + totalPages + " pages until you give up on a book");
     }
 }
